f168e5831b536369c4dadd52fda9e7b93711f238
Margarita_Filimonova <filimonovamargarita@mail.ru>
2018-01-31 14:11:41 +0300

    init file edited

--- src/main/java/base/TestMetalsAndColorsInit.java
+++ src/main/java/base/TestMetalsAndColorsInit.java
@@ -10,6 +10,7 @@ import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
 public class TestMetalsAndColorsInit extends TestNGBase {
     @BeforeSuite(alwaysRun = true)
     public static void setUp() throws Exception{
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebSite.init(SameElementsOfSite.class);
         logger.info("Run Tests");
     }
